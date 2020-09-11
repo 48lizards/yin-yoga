8cded66abb591571953d6aa6e3a153250bdb0eae
Jonathan Beller <beller.jonathan@gmail.com>
2020-09-10 21:01:18 -0400

    Updates

---
+++ precache-manifest.da8f238cd93127e6e4599dd82e8caa77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df889cc777639ea99bb7853b4f78a7e",
+    "url": "/yin-yoga/index.html"
+  },
+  {
+    "revision": "dd132826e8ca9e618d1a",
+    "url": "/yin-yoga/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6c0b76ba24841b5c5f50",
+    "url": "/yin-yoga/static/js/2.a1f4bd85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yin-yoga/static/js/2.a1f4bd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2902f164c583884c2cc2",
+    "url": "/yin-yoga/static/js/3.02dddf2b.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/yin-yoga/static/js/3.02dddf2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd132826e8ca9e618d1a",
+    "url": "/yin-yoga/static/js/main.784ec24a.chunk.js"
+  },
+  {
+    "revision": "21fa2db9151d488fa029",
+    "url": "/yin-yoga/static/js/runtime-main.785895b7.js"
+  },
+  {
+    "revision": "046393f2944fa4b21b0be177f282567d",
+    "url": "/yin-yoga/static/media/background.046393f2.jpg"
+  },
+  {
+    "revision": "66dd8682558454106dc06cabf81e6f40",
+    "url": "/yin-yoga/static/media/beep.66dd8682.mp3"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/yin-yoga/precache-manifest.da8f238cd93127e6e4599dd82e8caa77.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yin-yoga/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
