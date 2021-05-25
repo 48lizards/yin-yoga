e5c461713e5cd1a13421cc676620b7ecfad42283
Jonathan Beller <beller.jonathan@gmail.com>
2021-05-24 22:23:15 -0400

    Updates

---
+++ precache-manifest.c380691dbc2fb35d852336fe171310c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613a919982bfd635c9cd69d949cc26fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "407cfd119cc9e99e97d2",
+    "url": "/static/css/main.4f19d1b9.chunk.css"
+  },
+  {
+    "revision": "80adf87728878f7256c0",
+    "url": "/static/js/2.34b792f6.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.34b792f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407cfd119cc9e99e97d2",
+    "url": "/static/js/main.37a35972.chunk.js"
+  },
+  {
+    "revision": "ad919996184d643233e9",
+    "url": "/static/js/runtime-main.6334d81b.js"
+  },
+  {
+    "revision": "22be61396f729519d373f6d32cf52436",
+    "url": "/static/media/NotoMono-Regular.22be6139.ttf"
+  },
+  {
+    "revision": "295ce941f718b7d7e13f4cee67cea691",
+    "url": "/static/media/bannerlogo.295ce941.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yin-yoga-timer/precache-manifest.7aff8648ec6208958aaefe14f887ca38.js"
+  "/precache-manifest.c380691dbc2fb35d852336fe171310c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yin-yoga-timer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
